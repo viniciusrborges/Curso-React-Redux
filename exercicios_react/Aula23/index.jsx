042ecdc777809d977af9b382525a96999f53b929
viniciusrborges <vinicius_borges@live.com>
2018-01-25 23:51:23 -0200

    Initial commit

---
+++ exercicios_react/Aula23/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+//import Component, { Primeiro, Segundo } from './component'
+import Primeiro, { Segundo } from './component'
+
+ReactDOM.render(
+    <div>
+        <Primeiro/>
+        <Segundo/>
+    </div>
+, document.getElementById('app'))
