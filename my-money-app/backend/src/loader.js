7d8120f6317f207ad904701ee34015360fe68dad
viniciusrborges <vinicius_borges@live.com>
2018-02-04 20:48:38 -0200

    Aula91

--- my-money-app/backend/src/loader.js
+++ my-money-app/backend/src/loader.js
@@ -1,4 +1,3 @@
-require('./config/server')
-// const server = require('./config/server')
+const server = require('./config/server')
 require('./config/database')
-// require('./config/routes')(server)
+require('./config/routes')(server)
