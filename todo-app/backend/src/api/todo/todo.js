ca8ddf5ac6c4eceb256c7f994dad4f7cdc253a4c
viniciusrborges <vinicius_borges@live.com>
2018-02-04 23:09:14 -0200

    Aula92

--- todo-app/backend/src/api/todo/todo.js
+++ todo-app/backend/src/api/todo/todo.js
@@ -2,8 +2,8 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const todoSchema = new mongoose.Schema({
-    description: { type: String, require: true },
-    done: { type: Boolean, require: true, default: false },
+    description: { type: String, required: true },
+    done: { type: Boolean, required: true, default: false },
     createdAt: { type: Date, default: Date.now }
 })
 
