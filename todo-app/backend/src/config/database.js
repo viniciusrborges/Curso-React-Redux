f55708692d269a12c23a42ff8fb429bfc3d28048
viniciusrborges <vinicius_borges@live.com>
2018-03-16 19:50:02 -0300

    changeDataBase

--- todo-app/backend/src/config/database.js
+++ todo-app/backend/src/config/database.js
@@ -1,3 +1,4 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/todo')
+//module.exports = mongoose.connect('mongodb://localhost/todo')
+module.exports = mongoose.connect('mongodb://vinicius:d27m10@ds215709.mlab.com:15709/todo')
