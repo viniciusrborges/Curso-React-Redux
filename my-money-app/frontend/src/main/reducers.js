cdd41185dd54a1aa7863a2ddf1b0262cfec95b25
viniciusrborges <vinicius_borges@live.com>
2018-03-18 21:51:33 -0200

    Aula132

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -3,14 +3,14 @@ import { combineReducers } from 'redux'
 // import { reducer as toastrReducer } from 'react-redux-toastr'
 
 import DashboardReducer from '../dashboard/dashboardReducer'
-// import TabReducer from '../common/tab/tabReducer'
-// import BillingCycleReducer from '../billingCycle/billingCycleReducer'
+import TabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducer = combineReducers({
     //dashboard: () => ({summary: {credit: 200, debt: 50}})
-    dashboard: DashboardReducer/*,
+    dashboard: DashboardReducer,
     tab: TabReducer,
-    billingCycle: BillingCycleReducer,
+    billingCycle: BillingCycleReducer/*,
     form: formReducer,
     toastr: toastrReducer*/
 })
