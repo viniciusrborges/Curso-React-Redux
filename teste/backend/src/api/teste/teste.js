569316b9d812054708dbbc2e1b024144d912b82e
viniciusrborges <vinicius_borges@live.com>
2018-03-25 13:41:56 -0300

    createdAt no json

--- teste/backend/src/api/teste/teste.js
+++ teste/backend/src/api/teste/teste.js
@@ -6,10 +6,12 @@ const consumoSchema = new mongoose.Schema({
 })
 
 const testeSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    value: { type: Number, min: 0, required: true },
+    createdAt: { type: Date, default: Date.now }
+    /*name: { type: String, required: true },
     month: { type: Number, min: 1, max: 12, required: true },
     year: { type: Number, min: 1970, max: 2100, required: true },
-    consumo: [consumoSchema]
+    consumo: [consumoSchema]*/
 })
 
 module.exports = restful.model('teste', testeSchema)
